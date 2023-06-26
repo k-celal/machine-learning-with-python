5c5ffe9b4b1858b6bca2caea6ed764baed73bb52
Celal Karahan <celal51kara@gmail.com>
2023-06-27 02:12:02 +0300

    update

---
+++ 27-XGBoost/main.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 22 14:02:27 2023
+
+@author: Celal
+"""
+
+#1)Gerekli kutuphaneler
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#2)veri onisleme
+#2.1)Verilerin yuklenmesi
+veriler = pd.read_csv("Churn_modelling.csv")
+print(veriler)
+#rownumber , customerid,surname gereksiz sütunlar
+X=veriler.iloc[:,3:13].values
+Y=veriler.iloc[:,13].values
+
+#4)encoder kategorik veriler -> numeric veri
+from sklearn import preprocessing
+le=preprocessing.LabelEncoder()
+X[:,1]=le.fit_transform(X[:,1])
+le2=preprocessing.LabelEncoder()
+X[:,2]=le2.fit_transform(X[:,2])
+
+#kolonları 1 veya 0 atamak
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+ohe=ColumnTransformer([("ohe",OneHotEncoder(dtype=float),[1])],remainder="passthrough")
+X=ohe.fit_transform(X)
+X=X[:,1:]
+
+
+#verilerin bolunmesi
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,Y,test_size=0.33,random_state=0)
+
+from xgboost import XGBClassifier
+
+classifier=XGBClassifier()
+classifier.fit(X_train,y_train)
+
+y_pred=classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+
+cm=confusion_matrix(y_pred,y_test)
+print(cm)
+
+
+
+
+
+
